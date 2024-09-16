50919ffaa2b0f30b43768fff8105f18f9cafd017
Silver1104 <raghavpoddar39@gmail.com>
2024-09-17 04:19:11 +0530

    eigth commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {ConvexAuthProvider} from "@convex-dev/auth/react"
 import './index.css'
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
-
+console.log(`ID here: `, convex);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
       <ConvexAuthProvider client={convex}>
